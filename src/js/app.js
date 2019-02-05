44f7eaee4a0c4da5e6064d22d4e90cfe52fc7f94
Chema <chemar@gmail.com>
2019-02-05 15:24:41 +0100

    create init app

--- src/js/app.js
+++ src/js/app.js
@@ -1,15 +1,29 @@
-var canvas = null,
-    ctx = null;
+import config from './config.js'
 
-function paint(ctx) {
-    ctx.fillStyle = '#0f0';
-    ctx.fillRect(50, 50, 100, 60);
+class App {
+  constructor (idCanvas) {
+    let {id, context} = config.canvas
+    this.canvasId = idCanvas || id
+    this.canvas = document.getElementById(this.canvasId) ? document.getElementById(this.canvasId) : this.createCanvas()
+    this.ctx = this.canvas.getContext(context)
+    this.paint()
+  }
+
+  createCanvas () {
+    let {id, width, height} = config.canvas
+    let element = document.createElement('canvas')
+    element.setAttribute('id', id)
+    element.setAttribute('width', width)
+    element.setAttribute('height', height)
+    element.innerHTML = 'Canvas not supported'
+    return document.body.appendChild(element)
+  }
+
+  paint () {
+    let { ctx } = this
+    ctx.fillStyle = '#0f0'
+    ctx.fillRect(50, 50, 100, 60)
+  }
 }
 
-function init() {
-    canvas = document.getElementById('canvas');
-    ctx = canvas.getContext('2d');
-    paint(ctx);
-}
-
-window.addEventListener('load', init, false);
+window.app = new App()
---
+++ src/js/config.js
@@ -0,0 +1,8 @@
+export default {
+  canvas: {
+    id: '_id_001',
+    width: 300,
+    height: 150,
+    context: '2d'
+  }
+}
